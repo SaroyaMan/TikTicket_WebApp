8c9d802d520b3dd2237e409744c775ae4b3a67ec
Yoav Saroya <stankovic100@gmail.com>
2017-10-05 13:13:29 +0300

    Updating README.MD

--- docs/includes/logout.php
+++
@@ -1,4 +0,0 @@
-<?php
-session_start();
-$_SESSION['user_id'] = -1;  //Is Used To Destroy Specified Session
-header("location: ../login.html");
--- docs/includes/session.php
+++
@@ -1,5 +0,0 @@
-<?php
-session_start();// Starting Session
-if(!isset($_SESSION['user_id']) || $_SESSION['user_id'] == -1) {
-    header("location: login.html");
-}
